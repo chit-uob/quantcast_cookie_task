4636ea88d158c57595f49138e388a28e20ab1228
User <sample@email.com>
2022-05-19 14:34:29 +0100

    added logging of results so that it doesn't have to be ran twice if the previous run's result was forgotten

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -4,10 +4,13 @@ import sys
 import file_reader
 from arg_parse import parse_argument
 import logging
-logging.basicConfig(filename='usage.log')
+logging.basicConfig(filename='usage.log', filemode="a", level=logging.INFO)
 
 if __name__ == "__main__":
     cookie_path, date_string = parse_argument(sys.argv[1:])
+    logging.info(f"Called with arguments {sys.argv[1:]}")
     most_active_cookie_list = file_reader.handle_file(cookie_path, date_string)
+    logging.info(f"Results are:\n")
     for cookie in most_active_cookie_list:
         print(cookie)
+        logging.info(cookie)
